183af3f2f0ccda8a1a3041c7f9c8f36d71e41119
Umair-ahmad1 <ua225@cam.ac.uk>
2022-01-30 18:58:12 +0000

    task 1 b done

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -9,23 +9,30 @@ geographical data.
 
 from floodsystem.station import MonitoringStation
 from .utils import sorted_by_key  # noqa
+from math import radians, cos, sin, asin, sqrt
 
-from haversine import haversine
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance in kilometers between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return c * r
 
 
 def stations_by_distance(stations,p):
-    p=(52.053,0.1218)
-    distance=(0,0)
+    distance=[]
     for station in stations:
-        distance = haversine(p,stations.coord)
-    x=sorted_by_key([(stations.name),(stations.town),(distance)],2)
+        distance.append((station, haversine(p[0],p[1],station.coord[0],station.coord[1])))
+    x=sorted_by_key(distance,1)
     
     return x
 
-def stations_by_radius(stations,centre,p):
-    for station in stations:
-        if distance <10:
-            #add to list 
-
-
-    
