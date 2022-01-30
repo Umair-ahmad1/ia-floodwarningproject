fe0a4c29832435703e4896ad71bcda8e30e362f4
Umair-ahmad1 <ua225@cam.ac.uk>
2022-01-30 14:42:58 +0000

    first functino

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -8,15 +8,19 @@ geographical data.
 
 from floodsystem.station import MonitoringStation
 from .utils import sorted_by_key  # noqa
+pip install haversine
+from haversine import haversine, km
+
 
 def stations_by_distance(stations,p):
     p=(52.053,0.1218)
-    #stations will be a list of data then take the relevant parts of the data for each part of the formula
-    #then with p, use the haversine formula to give the distance away from cambridge
-    #so create a new list taking the station name,town,and the calculate distance away from p above
-    #sort this final list with the sorted_by_key function to sort by the distance
-    #for task 1b
-
+    distance=(0,0)
+    for station in stations:
+        distance = haversine(p,stations.coord)
+    x=sorted_by_key([(stations.name),(stations.town),(distance)]2)
+    
+    print(x)
+return
 def stations_within_radius(stations,centre,r):
     r=10
     centre=(52.053,0.1218)
@@ -25,5 +29,6 @@ def stations_within_radius(stations,centre,r):
     #if the haversine formula result is greater than the radius do not add to the list
     #then sort the results by alphabetic, so by the first part of the list
     #for 1c
+return
 
     
