44bd7ffbfcaea2935d0619b2942401d3541c3972
Umair-ahmad1 <ua225@cam.ac.uk>
2022-01-30 14:48:45 +0000

    function 1

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,10 +5,9 @@
 geographical data.
 
 """
-
 from floodsystem.station import MonitoringStation
 from .utils import sorted_by_key  # noqa
-pip install haversine
+
 from haversine import haversine, km
 
 
@@ -19,16 +18,7 @@ def stations_by_distance(stations,p):
         distance = haversine(p,stations.coord)
     x=sorted_by_key([(stations.name),(stations.town),(distance)]2)
     
-    print(x)
-return
-def stations_within_radius(stations,centre,r):
-    r=10
-    centre=(52.053,0.1218)
-    #stations will contain the names and the geographic co-ordinate
-    #station geographic co-ordinate used again with the haversine formula
-    #if the haversine formula result is greater than the radius do not add to the list
-    #then sort the results by alphabetic, so by the first part of the list
-    #for 1c
-return
+    return x
+
 
     
