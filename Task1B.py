03ceaa701b53952f71c48f80d61275d9685074a9
Umair-ahmad1 <ua225@cam.ac.uk>
2022-01-30 15:30:42 +0000

    o

--- Task1B.py
+++ Task1B.py
@@ -1,3 +1,7 @@
 #use the function created in the geo section station_by_distance
 #print a list of the first 10 values 
 #print a list of the last 10 values
+from floodsystem.geo import stations_by_distance
+from haversine import haversine 
+
+print(stations_by_distance)
