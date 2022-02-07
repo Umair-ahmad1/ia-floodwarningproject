be96ae8e3a4ddef8174e8cd5fe642803d8ca4f46
Umair-ahmad1 <ua225@cam.ac.uk>
2022-02-07 20:11:58 +0000

    1c finished

--- Task1C.py
+++ Task1C.py
@@ -1,8 +1,10 @@
 #print the second function from geo stations_within_radius, giving a value for r as 10
 from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
+from floodsystem.utils import sorted_by_key
 stations = build_station_list()
 centre=(52.2053,0.1218)
-r=10
-radial_distances = stations_within_radius(stations,centre,r)
-print(radial_distances)
+radial_distances = stations_within_radius(stations,centre,10)
+print("stations in radius of 10km from Cambridge city centre")
+x=(sorted_by_key(radial_distances, 0))
+print(x)
