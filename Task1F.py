fb85f231e19f678c4a11118a5e6d05adfe952f8c
Umair-ahmad1 <ua225@cam.ac.uk>
2022-02-07 20:19:02 +0000

    1f beginning

--- Task1F.py
+++ Task1F.py
@@ -1,2 +1,9 @@
 from floodsystem.station import inconsistent_typical_range_stations
-print()
+from floodsystem.stationdata import build_station_list
+from floodsystem.utils import sorted_by_key
+
+
+stations=build_station_list()
+inconsistent_data= inconsistent_typical_range_stations(stations)
+y= sorted_by_key(inconsistent_data,0)
+print(y)
