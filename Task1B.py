fd29d1d6898a032731f401c1b7f81192b2de56c7
Umair-ahmad1 <ua225@cam.ac.uk>
2022-02-08 14:08:07 +0000

    added in the requisite syntax that the deliverables asked for

--- Task1B.py
+++ Task1B.py
@@ -3,13 +3,17 @@
 #print a list of the last 10 values
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
-stations = build_station_list()
-p=(52.2053,0.1218)
-distances = stations_by_distance(stations, p)
-print("10 closest stations")
-for x in range(10):
-    print(distances[x][0].name,distances[x][0].town,distances[x][1])
-print("10 furthest away stations")
-for x in range(len(distances)-10, len(distances)):
-    #
-    print(distances[x][0].name,distances[x][0].town,distances[x][1])
+def run(): 
+    stations = build_station_list()
+    p=(52.2053,0.1218)
+    distances = stations_by_distance(stations, p)
+    print("10 closest stations")
+    for x in range(10):
+        print(distances[x][0].name,distances[x][0].town,distances[x][1])
+    print("10 furthest away stations")
+    for x in range(len(distances)-10, len(distances)):    
+        print(distances[x][0].name,distances[x][0].town,distances[x][1])
+
+if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    run()
